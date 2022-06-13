65e7084a4dd9aa2103a4742f49ac66db33a596c3
Jonathan Allenberg <git@jfa.sh>
2022-06-13 13:50:54 +0200

    stories

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,147 @@
 const rootMain = require('../../../../.storybook/main')
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
 
-// Use the following syntax to add addons!
-// rootMain.addons.push('');
-rootMain.stories.push(
-  ...['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)']
-)
+module.exports = {
+  ...rootMain,
+  core: { ...rootMain.core, builder: 'webpack5' },
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  webpackFinal: async (baseConfig, { configType }) => {
+    const svgRuleIndex = baseConfig.module.rules.findIndex((rule) => {
+      const { test } = rule
+      return test.toString().startsWith('/\\.(svg|ico')
+    })
 
-module.exports = rootMain
+    baseConfig.module.rules[
+      svgRuleIndex
+    ].test = /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|cur|ani|pdf)(\?.*)?$/
+
+
+    return {
+      ...baseConfig,
+      resolve: {
+        ...baseConfig.resolve,
+        plugins: [
+          // ...baseConfig.resolve.plugins,
+          new TsconfigPathsPlugin({
+            configFile: 'tsconfig.base.json',
+            baseUrl: process.cwd()
+          })
+        ]
+      },
+      module: {
+        ...baseConfig.module,
+        rules: [
+          ...baseConfig.module.rules
+          // {
+          //   test: /\.(png|jpe?g|gif|webp)$/,
+          //   loader: require.resolve('url-loader'),
+          //   options: {
+          //     limit: 10000, // 10kB
+          //     name: '[name].[hash:7].[ext]'
+          //   }
+          // },
+          // {
+          //   test: /\.svg$/,
+          //   oneOf: [
+          //     // If coming from JS/TS file, then transform into React component using SVGR.
+          //     {
+          //       issuer: { test: /\.[jt]sx?$/ },
+          //       use: [
+          //         '@svgr/webpack?-svgo,+titleProp,+ref![path]',
+          //         {
+          //           loader: require.resolve('url-loader'),
+          //           options: {
+          //             limit: 10000, // 10kB
+          //             name: '[name].[hash:7].[ext]',
+          //             esModule: false
+          //           }
+          //         }
+          //       ]
+          //     },
+          //     // Fallback to plain URL loader.
+          //     {
+          //       use: [
+          //         {
+          //           loader: require.resolve('url-loader'),
+          //           options: { limit: 10000, name: '[name].[hash:7].[ext]' }
+          //         }
+          //       ]
+          //     }
+          //   ]
+          // }
+        ]
+      }
+    }
+  }
+}
+
+// const finalWebpack = async ({ config, mode }) => {
+//   config = await rootWebpackConfig({ config, mode })
+
+//   // const tsPaths = new TsconfigPathsPlugin({
+//   //   configFile: './tsconfig.base.json'
+//   // })
+
+//   // config.resolve.plugins
+//   //   ? config.resolve.plugins.push(tsPaths)
+//   //   : (config.resolve.plugins = [tsPaths])
+
+//   // Found this here: https://github.com/nrwl/nx/issues/2859
+//   // And copied the part of the solution that made it work
+
+//   // const svgRuleIndex = config.module.rules.findIndex((rule) => {
+//   //   const { test } = rule
+
+//   //   return test.toString().startsWith('/\\.(svg|ico')
+//   // })
+//   // config.module.rules[
+//   //   svgRuleIndex
+//   // ].test = /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|cur|ani|pdf)(\?.*)?$/
+
+//   config.module.rules.push(
+//     // {
+//     //   test: /\.(png|jpe?g|gif|webp)$/,
+//     //   loader: require.resolve('url-loader'),
+//     //   options: {
+//     //     limit: 10000, // 10kB
+//     //     name: '[name].[hash:7].[ext]'
+//     //   }
+//     // },
+//     {
+//       test: /\.svg$/,
+//       oneOf: [
+//         // If coming from JS/TS file, then transform into React component using SVGR.
+//         {
+//           issuer: {
+//             test: /\.[jt]sx?$/
+//           },
+//           use: [
+//             '@svgr/webpack?-svgo,+titleProp,+ref![path]',
+//             {
+//               loader: require.resolve('url-loader'),
+//               options: {
+//                 limit: 10000, // 10kB
+//                 name: '[name].[hash:7].[ext]',
+//                 esModule: false
+//               }
+//             }
+//           ]
+//         },
+//         // Fallback to plain URL loader.
+//         {
+//           use: [
+//             {
+//               loader: require.resolve('url-loader'),
+//               options: {
+//                 limit: 10000, // 10kB
+//                 name: '[name].[hash:7].[ext]'
+//               }
+//             }
+//           ]
+//         }
+//       ]
+//     }
+//   )
+
+//   return config
+// }
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,2 @@
-import { addDecorator } from '@storybook/react';
-import { withKnobs } from '@storybook/addon-knobs';
+// import { addDecorator } from '@storybook/react';
 
-addDecorator(withKnobs);
