51467b2d29ae2024dfe487defbee80a0f8d04c4b
Jonathan Allenberg <git@jfa.sh>
2022-06-13 13:51:15 +0200

    ts and jest configs

--- jest.config.ts
+++ jest.config.ts
@@ -1,12 +1,13 @@
-module.exports = {
+/* eslint-disable */
+export default {
   displayName: 'view-graph',
   preset: '../../../jest.preset.js',
   transform: {
     '^.+\\.[tj]sx?$': [
       'babel-jest',
-      { cwd: __dirname, configFile: './babel-jest.config.json' },
-    ],
+      { cwd: __dirname, configFile: './babel-jest.config.json' }
+    ]
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../../coverage/packages/view/graph',
-};
+  coverageDirectory: '../../../coverage/packages/view/graph'
+}
